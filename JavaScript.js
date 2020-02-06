1ad906475b0514e7fbb74cd705e9df5ca3396622
Maxkamov Alisher <media.art.press2@gmail.com>
2020-02-06 20:37:43 +0500

    HW-1

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+var Tc = prompt("Задать температуру в градусах по Цельсию °C:");
+var Tf= (9 / 5) * Tc + 32;
+ alert("Tемпература по Фаренгейту: " + Tf + "°F");
+
+
+var name = "Василий";
+var admin = name;
+alert(admin);
+
